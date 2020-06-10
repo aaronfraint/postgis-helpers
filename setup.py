3c040e2f986d42d3b4c6e4552674cd8ae0d10573
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-06-09 20:09:19 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='postgis_helpers',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Python helpers that facilitate SQL data I/O',
+    author='Aaron Fraint, AICP',
+    license='MIT',
+)
