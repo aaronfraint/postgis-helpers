e4a05240514dd1d561875a4c9a7120082bfa5a89
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-06-13 09:41:18 -0700

    Create test__make_geotable.py

---
+++ postgis_helpers/tests/test__make_geotable.py
@@ -0,0 +1,34 @@
+from ward import test, using
+
+from postgis_helpers import PostgreSQL
+from postgis_helpers.tests.fixtures import DataForTest, database_local, test_shp_data
+
+
+# Can we make new geometric tables from queries?
+# ---------- ---------- ---------- ---------- --
+def _test_make_geotable_from_query(db: PostgreSQL,
+                                   shp: DataForTest):
+
+    new_geotable = "test_make_geotable_multilinestring"
+
+    query = f"""
+        SELECT ST_UNION(geom) AS geom
+        FROM {shp.NAME}
+    """
+
+    # Make a new geotable
+    db.make_geotable_from_query(query,
+                                new_geotable,
+                                geom_type="MULTILINESTRING",
+                                epsg=shp.EPSG)
+
+    # Confirm that the new table's EPSG matches the expected value
+    epsg = db.all_spatial_tables_as_dict()[new_geotable]
+
+    assert epsg == shp.EPSG
+
+
+@test("PostgreSQL().make_geotable_from_query() creates a new spatial table with appropriate EPSG")
+@using(database=database_local, shp=test_shp_data)
+def _(database, shp):
+    _test_make_geotable_from_query(database, shp)
