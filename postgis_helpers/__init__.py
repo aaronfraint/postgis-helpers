94146da82a39259fb0329e6c2ded0ffdeb80eab2
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-06-22 15:45:30 -0700

    update import

--- postgis_helpers/__init__.py
+++ postgis_helpers/__init__.py
@@ -1,3 +1,3 @@
 from .PgSQL import PostgreSQL
-from .config_helpers import configurations
+from .config_helpers import make_config_file, read_config_file, configurations
 from .geopandas_helpers import spatialize_point_dataframe
