d8160aed37e0eaabfc105f0d7c147188fe9b087f
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-06-22 15:45:49 -0700

    make more modular

--- postgis_helpers/config_helpers.py
+++ postgis_helpers/config_helpers.py
@@ -1,20 +1,27 @@
-""" You may have lots of SQL database connections.
+""" 
+Summary of ``config_helpers.py``
+--------------------------------
+
+You may have lots of SQL database connections.
 Or maybe you just want to keep your database
 connection information out of your analysis
 code. This module provides a method for storing
 and retrieving connections to database clusters
 by named profiles inside ``[square_brackets]``.
 
+TODO: update this with new features
 """
-import os
 import configparser
 from pathlib import Path
+from typing import Union
 
 DATA_ROOT = Path.home() / "sql_data_io"
 DEFAULT_DATA_INBOX = DATA_ROOT / "inbox"
 DEFAULT_DATA_OUTBOX = DATA_ROOT / "outbox"
 
-starter_config_file = """
+DB_CONFIG_FILEPATH = DATA_ROOT / "database_connections.cfg"
+
+STARTER_CONFIG_FILE = """
 [DEFAULT]
 pw = this-is-a-placeholder-password
 port = 5432
@@ -39,19 +46,50 @@ super_pw = some_super_password12354
 """
 
 
-def configurations(verbosity: str = "full") -> dict:
+def make_config_file(filepath: Union[Path, str] = DB_CONFIG_FILEPATH,
+                     overwrite: bool = False) -> bool:
 
-    config_file = os.path.join(Path.home(), ".postgis_helpers")
+    filepath = Path(filepath)
 
-    print(f"Loading {config_file}")
-    if not os.path.exists(config_file):
-        print("Config file does not yet exist")
-        print(f"Writing default config file to {config_file}")
-        with open(config_file, "w") as open_file:
-            open_file.write(starter_config_file)
+    if not overwrite and filepath.exists():
+        print(f"\n -> overwrite=False and {filepath} exists. Will not overwrite.")
+        return False
 
-    # Parse the config file saved to /Users/yourname/.postgis_helpers
+    else:
+        if filepath.exists():
+            print(f"\n -> Overwriting config file at {filepath}")
+        else:
+            print(f"\n -> Creating a new config file at {filepath}")
+
+        with open(filepath, "w") as open_file:
+            open_file.write(STARTER_CONFIG_FILE)
+
+        return True
+
+
+def read_config_file(filepath: Path = DB_CONFIG_FILEPATH) -> dict:
+
     config = configparser.ConfigParser()
-    config.read(config_file)
+    config.read(filepath)
 
-    return {host: config[host] for host in config.sections()}
+    all_hosts = {}
+
+    for host in config.sections():
+        all_hosts[host] = {}
+
+        for key in config[host]:
+            value = config[host][key]
+
+            all_hosts[host][key] = value
+
+    print(f"Loaded db configurations from {filepath}")
+
+    return all_hosts
+
+
+def configurations(filepath: Path = DB_CONFIG_FILEPATH) -> dict:
+
+    if not filepath.exists():
+        make_config_file(filepath)
+
+    return read_config_file(filepath)
