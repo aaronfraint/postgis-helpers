7448b8bafe01a9981fea6a889a1daecfd3a39bd6
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-07-09 16:23:47 -0700

    expose DataSource, console, and version

--- postgis_helpers/__init__.py
+++ postgis_helpers/__init__.py
@@ -1,3 +1,13 @@
 from .PgSQL import PostgreSQL
 from .config_helpers import make_config_file, read_config_file, configurations
 from .geopandas_helpers import spatialize_point_dataframe
+from .raw_data import DataSource
+
+from .console import _console
+
+__VERSION__ = "0.2.0"
+
+# _console.print(":globe_showing_americas:", justify="left")
+# _console.print(":globe_showing_europe-africa:", justify="center")
+# _console.print(":globe_showing_asia-australia:", justify="right")
+# _console.print(f"-> postGIS-helpers version {__VERSION__}\n\n")
