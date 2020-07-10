1545d7252336ea48d2b73b032bf6192a18c16db9
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-07-09 18:31:32 -0700

    bump version

--- postgis_helpers/__init__.py
+++ postgis_helpers/__init__.py
@@ -5,7 +5,7 @@ from .raw_data import DataSource
 
 from .console import _console
 
-__VERSION__ = "0.2.0"
+__VERSION__ = "0.2.1"
 
 # _console.print(":globe_showing_americas:", justify="left")
 # _console.print(":globe_showing_europe-africa:", justify="center")
