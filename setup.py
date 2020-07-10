e3a8ae319dfa3ae81311d192250bf707fa9a026a
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-07-09 18:34:23 -0700

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='postgis_helpers',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.2.1',
     description='Python helpers that facilitate SQL data I/O',
     author='Aaron Fraint, AICP',
     license='MIT',
